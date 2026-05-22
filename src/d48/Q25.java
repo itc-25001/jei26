6be4310980048c72f7ffbc9ccf7a69ce69091ee5
itc-s25001 <s25001@std.it-college.ac.jp>
2026-05-22 10:21:21 +0900

    課題

---
+++ src/d48/Q25.java
@@ -0,0 +1,16 @@
+package d48;
+
+public class Q25 {
+    String code;
+    public static void main(String[] args){
+        Q25 obj = new Q25();
+        obj.setCode("FB1257") // objのsetCodeメソッドを使い、文字列をセットする。
+    }
+    void setCode(String s){
+        code = s;
+    }
+}
+/*
+javaでは文字列を指定する場合 ""で囲う必要がある。
+''は一文字だけchar型を扱うときに使うので、混合しないようにしましょう。
+ */
