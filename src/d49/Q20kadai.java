6f6c28a573a314f50f4110c03faeae760a90cdaf
itc-s25001 <s25001@std.it-college.ac.jp>
2026-05-29 10:09:40 +0900

    課題

---
+++ src/d49/Q20kadai.java
@@ -0,0 +1,8 @@
+package d49;
+
+public class Q20kadai {
+    public static void main(String[] args){
+        int[] arr =new int[10]; //ここをdouble型の2次元配列にする
+        System.out.println(arr.length);
+    }
+}
