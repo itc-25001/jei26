015aede7996b71f6a72cd3637e2986c58d5ecf75
itc-s25001 <s25001@std.it-college.ac.jp>
2026-06-05 10:20:41 +0900

    課題

---
+++ src/d50/Q20Kadai.java
@@ -0,0 +1,10 @@
+package d50;
+
+public class Q20Kadai {
+    public static void main(String[] args){
+        int num = 1234;
+        String scale = num > 1000 ? "Narrow" : "Wide";
+
+        System.out.println(scale);
+    }
+}
