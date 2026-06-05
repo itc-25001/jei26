88fde1cd7524ec560f3f7bdd51c0e657d2b9cbcd
itc-s25001 <s25001@std.it-college.ac.jp>
2026-06-05 10:45:34 +0900

    課題

---
+++ src/d50/Q23.java
@@ -0,0 +1,13 @@
+package d50;
+
+public class Q23 {
+    String code;
+    public static void main(String[] args){
+        Q23 obj = new Q23();
+        obj.setCode("Code");
+        System.out.println(obj.code);
+    }
+    void setCode(String c){
+        code = c;
+    }
+}
