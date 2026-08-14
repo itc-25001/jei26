0f5b56537b17286805c521098f7265b82ad42837
itc-s25001 <s25001@std.it-college.ac.jp>
2026-08-14 09:50:42 +0900

    課題

--- src/d49/Q20kadai.java
+++ src/d49/Q20kadai.java
@@ -2,7 +2,7 @@ package d49;
 
 public class Q20kadai {
     public static void main(String[] args){
-        int[] arr =new int[10]; //ここをdouble型の2次元配列にする
+        double[][] arr =new double[5][10]; //ここをdouble型の2次元配列にする
         System.out.println(arr.length);
     }
 }
